88a2e455055a7935f6504ff02bade206b2307038
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-02 20:03:09 -0600

    Fixed the on change function, I believe I'm ready to move on to the next component?

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -58,7 +58,7 @@ const Search = ({ setIsLoading, setSearchResults }) => {
         type="text" 
         placeholder="enter keywords..." 
         value={queryString} 
-        onChange={e => setQueryString(e.target.value)}/>
+        onChange={(event) => setQueryString(event.target.value)}/>
     </fieldset>
     <fieldset>
       <label htmlFor="select-classification">Classification <span className="classification-count">({ classificationList.length })</span></label>
@@ -66,7 +66,7 @@ const Search = ({ setIsLoading, setSearchResults }) => {
         name="classification"
         id="select-classification"
         value={classification} 
-        onChange={e => setClassification(e.target.value)}>
+        onChange={(event) => setClassification(event.target.value)}>
         <option value="any">Any</option>
         {/* map over the classificationList, return an <option /> */}
       </select>
@@ -77,7 +77,7 @@ const Search = ({ setIsLoading, setSearchResults }) => {
         name="century" 
         id="select-century"
         value={century} 
-        onChange={e => setCentury(e.target.value)}>
+        onChange={(event) => setCentury(event.target.value)}>
         <option value="any">Any</option>
         {/* map over the centuryList, return an <option /> */}
       </select>
